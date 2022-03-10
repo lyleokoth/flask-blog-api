396d0adfd4678833a3b00335ac8eb709c659112c
lyleokoth <okothlyleochieng@gmail.com>
2022-03-10 13:55:09 +0300

    Created the production database.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from app.config import DevelopmentConfig, ProductionConfig
 from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+#app.config.from_object(DevelopmentConfig)
+app.config.from_object(ProductionConfig)
 
 db = MongoEngine(app)
 
--- app/config.py
+++ app/config.py
@@ -22,8 +22,8 @@ class ProductionConfig(BaseConfig):
     DEBUG = False
     TESTING = False 
     MONGODB_DB = 'flask-api'
-    MONGODB_HOST = '127.0.0.1'
+    MONGODB_HOST = 'mongodb+srv://cluster0.ctzy7.mongodb.net/'
     MONGODB_PORT = 27017
-    MONGODB_USERNAME = ''
-    MONGODB_PASSWORD = ''
+    MONGODB_USERNAME = 'flask-api'
+    MONGODB_PASSWORD = 'flask-api'
     MONGODB_CONNECT = True
